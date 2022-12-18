5cf72d4bd287ff06ffe4a5c1c90c775cdca23ac8
evergreen-trading-systems <4870868+evergreen-trading-systems@users.noreply.github.com>
2022-12-18 14:50:05 +0000

    refactor: add it

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use postal::{Components, InitOptions, ParseAddressOptions, PostalError};
+
+pub use postal::Component;
+pub use postal::Context;
+
+pub fn make_new() -> Result<Context, PostalError> {
+    let mut ctx = Context::new();
+    ctx.init(InitOptions {
+        expand_address: false,
+        parse_address: true,
+    })?;
+    Ok(ctx)
+}
+
+pub fn parse(
+    ctx: Context,
+    s: &str,
+) -> Result<Vec<(String, String)>, PostalError> {
+    let mut opts = ParseAddressOptions::new();
+    ctx.parse_address(s, &mut opts).map(|x| {
+        x.map(|s| (s.label.to_string(), s.value.to_string()))
+            .collect::<Vec<_>>()
+    })
+}
